bbcf115e7a595dfbc6969fe8a2d7f0ab17461b45
neethupath <pathnee222gmail.com>
2023-04-07 15:57:36 -0400

    match

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const findMatching=(drivers,name)=>
+{
+ const d=drivers.filter((driverName)=>((driverName.toLowerCase()  ===  name.toLowerCase())));
+return d;
+};
+function fuzzyMatch(drivers,name)
+{
+let leng=name.length;
+return drivers.filter(function (driverName) {
+return driverName.slice(0,leng) === (name)
+});
+}
+function matchName(drivers,name)
+{
+return drivers.filter(function (drivername) {
+return drivername.name.toLowerCase() === name.toLowerCase()
+});
+}
